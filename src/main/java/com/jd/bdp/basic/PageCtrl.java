d903571cea74f2c86ef7ef904e6a9cfb571ffe69
zhangrui <zhangrui25@jd.com>
2016-07-21 09:28:16 +0800

    no message

--- src/main/java/com/jd/bdp/basic/PageCtrl.java
+++ src/main/java/com/jd/bdp/basic/PageCtrl.java
@@ -7,9 +7,14 @@ public class PageCtrl<T> {
 	public List<T> datas ;
 	
 	public int totalPage ;		//总页数
-	public int totalSize ;		//总条数
+	public int totalSize ;		//总条数d 的
  	public int pageSize ;		//每页多少条
 	public int pageNo ;			//请求第几页
 	
 	
+	public static void main(String[] args) {
+		PageCtrl<String> p = new PageCtrl<String>();
+		PageCtrl<String> p1 = new PageCtrl<String>();
+ 
+	}
 }
--- src/main/java/com/jd/bdp/basic/PageCtrlInterceptor.java
+++ src/main/java/com/jd/bdp/basic/PageCtrlInterceptor.java
@@ -1,36 +1,41 @@
 package com.jd.bdp.basic;
 
+import java.sql.Connection;
+import java.util.Arrays;
 import java.util.Properties;
 
-import org.apache.ibatis.executor.Executor;
-import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
-import org.apache.ibatis.plugin.Plugin;
 import org.apache.ibatis.plugin.Signature;
 
 @Intercepts({@Signature(
-		  type= Executor.class,
-		  method = "select",
-		  args = {MappedStatement.class,Object.class})})
+		type=StatementHandler.class,
+		method="prepare",
+		args={Connection.class,Integer.class})
+	})  
 public class PageCtrlInterceptor implements Interceptor{
 
+	 
 	@Override
 	public Object intercept(Invocation invocation) throws Throwable {
-		
+		System.out.println("Args...." + Arrays.toString(invocation.getArgs()) );
+		System.out.println("Method : "+ invocation.getMethod());	
+		System.out.println("TargetClass : "+ invocation.getTarget().getClass().getName());	
  		return invocation.proceed();
 	}
 
 	@Override
 	public Object plugin(Object target) {
-		
- 		return  Plugin.wrap(target, this);
+		System.out.println("target Name : " + target.getClass().getName());
+ 		/*return  Plugin.wrap(target, this);*/
+		return target ;
 	}
 
 	@Override
 	public void setProperties(Properties properties) {
- 		
+		System.out.println("here....");
 	}
 
 }
